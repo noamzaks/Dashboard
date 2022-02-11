d4a34b6568a1fedaca36fed408f5715c2808d8a8
Noam Zaks <imnoamzaks@gmail.com>
2022-02-11 20:49:59 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import GridLayout from "react-grid-layout";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+interface DashboardWidget {
+    name: string;
+    type: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    static: boolean;
+}
+
+interface DashboardTab {
+    name: string;
+    widgets: DashboardWidget[];
+}
+
+interface DashboardSchema {
+    tabs: DashboardTab[];
+}
+
+const App = () => {
+    const [schema, setSchema] = useState<DashboardSchema>({
+        tabs: [],
+    });
+
+    useEffect(() => {
+        // @ts-ignore
+        window.setSchema = (schema: string) => setSchema(JSON.parse(schema));
+        // @ts-ignore
+        window.getSchema = () => JSON.stringify(schema, null, 4);
+    });
+
+    return (
+        <Tabs>
+            <TabList>
+                {schema.tabs.map((tab) => (
+                    <Tab key={tab.name}>{tab.name}</Tab>
+                ))}
+            </TabList>
+
+            {schema.tabs.map((tab) => (
+                <TabPanel>
+                    <GridLayout cols={4} rowHeight={89} width={800}>
+                        {tab.widgets.map((widget) => {
+                            return (
+                                <div
+                                    key={widget.name}
+                                    style={{
+                                        height: "100%",
+                                        width: "100%",
+                                        borderColor: "black",
+                                        borderWidth: 2,
+                                        borderRadius: 10,
+                                        borderStyle: "solid",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                    }}
+                                    data-grid={widget}
+                                >
+                                    <h1
+                                        style={{
+                                            textAlign: "center",
+                                            margin: 5,
+                                        }}
+                                    >
+                                        {widget.name}
+                                    </h1>
+                                    <hr />
+                                    <div
+                                        style={{
+                                            display: "flex",
+                                            flexGrow: 1,
+                                        }}
+                                    >
+                                        <div
+                                            style={{ margin: "auto" }}
+                                            dangerouslySetInnerHTML={{
+                                                __html: `<${widget.type} style="max-width: 100%; max-height: 100%;"></${widget.type}>`,
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </GridLayout>
+                </TabPanel>
+            ))}
+        </Tabs>
+    );
+};
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
